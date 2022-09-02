08bf743d21e0bb5b032b30fa92959a4899c7414e
SuperJuicer <Sara.Jade@alaskaair.com>
2022-09-02 13:24:18 -0700

    Remove solution and all code.

--- OrderProcessing.Customer/Data/ICustomerRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using OrderProcessing.Customer.Models;
-
-namespace OrderProcessing.Customer.Data
-{
-    public interface ICustomerRepository
-    {
-        Task<List<Person>> GetAllCustomers();
-    }
-}
